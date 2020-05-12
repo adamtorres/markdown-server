a70a0e2b392d1db9970805593161f7d7b9bf8f05
Adam Torres <adamprogrammer@gmail.com>
2020-05-11 23:39:56 -0600

    Added mdx_outline to see if it does what I want - collapse sections.

--- markdownserver/env.py
+++ markdownserver/env.py
@@ -13,4 +13,4 @@ html_dir = os.path.join(resource_dir, "html")
 css_dir = os.path.join(resource_dir, "css")
 css_name = "combined.css"
 css_path = os.path.join(css_dir, css_name)
-markdown_type = ["mdx_gfm","markdown.extensions.toc","markdown.extensions.def_list", "markdown.extensions.codehilite", ]
+markdown_type = ["mdx_gfm","markdown.extensions.toc","markdown.extensions.def_list", "markdown.extensions.codehilite", "mdx_outline"]
